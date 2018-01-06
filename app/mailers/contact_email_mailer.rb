ad981f5bf89c4897505d9917ac46b10f6b2c90b2
danfrenette <dan.r.frenette@gmail.com>
2018-01-06 12:28:41 -0500

    reset

--- app/mailers/contact_email_mailer.rb
+++ app/mailers/contact_email_mailer.rb
@@ -2,6 +2,7 @@ class ContactEmailMailer < ApplicationMailer
   def recieve_from_user(contact_email)
     @contact_email = contact_email
     @to = "dan.r.frenette@gmail.com"
+
     mail(to: @to, from: contact_email.email, subject: contact_email.subject)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module DanfrenetteCodes
     config.load_defaults 5.1
     config.autoload_paths += %W(#{config.root}/facades)
 
+
+    config.action_mailer.asset_host = "https://danfrenette.herokuapp.com/"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
